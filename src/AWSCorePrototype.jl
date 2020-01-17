8265c989264407743967c2a9134b7a842fcfb1d5
Matt Brzezinski <matt.brzezinski@invenia.ca>
2020-01-17 11:36:52 -0600

    Made request functions for Query, JSON and REST JSON services

--- src/AWSCorePrototype.jl
+++ src/AWSCorePrototype.jl
@@ -2,10 +2,6 @@ module AWSCorePrototype
 
 using AWSCore
 
-# Additional structs for:
-# - Query
-# - JSON
-# - RestJSON
 mutable struct RestXMLService
     name::String
     api_version::String
@@ -40,5 +36,43 @@ function (service::RestXMLService)(aws::AWSConfig, request_method::String, reque
 end
 (service::RestXMLService)(operation, args=[]) = service(default_aws_config(), operation, args)
 (service::RestXMLService)(a...; b...) = service(a..., b)
+
+function (service::QueryService)(aws::AWSConfig, operation, args=[])
+    return AWSCore.service_query(
+        aws;
+        service=service.name,
+        version=service.api_version,
+        operation=operation,
+        args=args
+    )
+(service::QueryService)(operation, args=[]) = service(default_aws_config(), operation, args)
+(service::QueryService)(a...; b...) = service(a..., b)
+
+function (service::JSONService)(aws::AWSConfig, operation, args=[])
+    return AWSCore.service_json(
+        aws;
+        service=service.name,
+        version=service.api_version,
+        json_version=service.json_version,
+        target=service.target,
+        operation=operation,
+        args=args
+    )
+end
+(service::JSONService)(operation, args=[]) = service(default_aws_config(), operation, args)
+(service::JSONService)(a...; b...) = service(a..., b)
+
+function (service::RestJSONService)(aws::AWSConfig, request_method::String, request_uri::String, args=[])
+    return AWSCore.service_rest_json(
+        aws;
+        service=service.name,
+        version=service.api_version,
+        verb=request_method,
+        resource=request_uri,
+        args=args
+    )
+end
+(service::RestJSONService)(request_method::String, request_uri::String, args=[]) = service(default_aws_config(), request_method, request_uri, args)
+(service::RestJSONService)(a...; b...) = service(a..., b)
 
 end  # module AWSCorePrototype
